60088d8adaf323651d2d2f2f7354d053044b0ffb
刘佳慧 <867467186@qq.com>
2021-11-19 14:59:44 +0800

    性能优化

---
+++ src/main-prod.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// import './plugins/element.js'
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入富文本
+import VueQuillEditor from 'vue-quill-editor'
+
+import NProgress from 'nprogress'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在 request 拦截器中，展示进度条 NProgress.start()
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+// 在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor)
+
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal * 1000)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
